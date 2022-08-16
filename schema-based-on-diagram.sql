7a8bb0f447b7f6e191136c9b5d491a4035c7e45d
dqarias <dino.quispea@gmail.com>
2022-08-16 15:48:56 -0500

    Update schema and create new index

--- schema-based-on-diagram.sql
+++ schema-based-on-diagram.sql
@@ -1,9 +1,10 @@
+CREATE DATABASE clinic;
+
 CREATE TABLE patients(
     id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     name VARCHAR(255) NOT NULL,
     date_of_birth INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    ON DELETE CASCADE
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE medical_histories(
@@ -12,7 +13,7 @@ CREATE TABLE medical_histories(
     patient_id INTEGER NOT NULL,
     status VARCHAR(255) NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT medical_histories_patient_id_foreign FOREIGN KEY(patient_id) REFERENCES patients(id);
+    CONSTRAINT medical_histories_patient_id_foreign FOREIGN KEY(patient_id) REFERENCES patients(id)
     ON DELETE CASCADE
 );
 
@@ -20,7 +21,7 @@ CREATE TABLE treatments(
     id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     type VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE invoices(
@@ -30,9 +31,10 @@ CREATE TABLE invoices(
     payed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     medical_history_id INTEGER NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
     ON DELETE CASCADE
 );
+
 CREATE TABLE invoice_items(
     id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     unit_price DECIMAL(8, 2) NOT NULL,
@@ -42,7 +44,7 @@ CREATE TABLE invoice_items(
     treatment_id INTEGER NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id),
-    CONSTRAINT invoice_items_invoice_id_foreign FOREIGN KEY(invoice_id) REFERENCES invoices(id),        
+    CONSTRAINT invoice_items_invoice_id_foreign FOREIGN KEY(invoice_id) REFERENCES invoices(id)        
     ON DELETE CASCADE
 );
 
@@ -50,8 +52,13 @@ CREATE TABLE treatments_histories (
     medical_histories_id INT NOT NULL,
     treatments_id INT NOT NULL,
     CONSTRAINT fk_treatments FOREIGN KEY(treatments_id) REFERENCES treatments(id),
-    CONSTRAINT fk_medical_histories FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id),
+    CONSTRAINT fk_medical_histories FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id)
     ON DELETE CASCADE
 );
 
 CREATE INDEX patient_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
+CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
+CREATE INDEX treatments_id_asc ON treatments_histories(treatments_id ASC);
+CREATE INDEX medical_histories_id_asc ON treatments_histories(medical_histories_id ASC);
